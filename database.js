ae924f00782196614c92699126a0a2e109a1bead
SithilSemitha <sithilsemithad@gmail.com>
2026-01-20 20:48:15 +0530

    feat: Added a demo database file

---
+++ database.js
@@ -0,0 +1,10 @@
+const users=[
+     { id: '1', name: 'Sithil', username: 'semi', password: '1234', role: 'customer' },
+    { id: '2', name: 'David', username: 'david', password: '1234', role: 'customer' },
+    { id: '3', name: 'John', username: 'john', password: '1234', role: 'customer' },
+    { id: '4', name: 'Nishanthan', username: 'nishanthan', password: '1234', role: 'customer' },
+    { id: '5', name: 'Pasindu', username: 'pasindu', password: '1234', role: 'customer' },
+    { id: '6', name: 'Sahan', username: 'sahan', password: '1234', role: 'admin' },
+];
+
+module.exports={users};
